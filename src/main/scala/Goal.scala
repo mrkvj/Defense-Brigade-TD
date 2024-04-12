073fa61a79b55dba36b3e83dc43fbf50126996bc
Veikko Markkula <veikko.markkula@aalto.fi>
2024-04-12 15:48:14 +0300

    Goal defined and improved.

--- src/main/scala/Goal.scala
+++ src/main/scala/Goal.scala
@@ -1,10 +1,35 @@
-package DefenseBrigadeTD
+package scala
 
-class Goal:
-  val default = 0
+import scalafx.scene.paint.Color.*
 
-  var x = default
-  var y = default
+import scala.collection.mutable.Buffer
+import scalafx.scene.canvas.GraphicsContext
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.{FillRule, LineTo, MoveTo, Path}
 
+import scala.collection.immutable.List
+class Goal(var place: Vector2D):
+
+  val side = goalSide
+  val color = LightGreen
+
+
+  def isInsideGoal(xPos: Double, yPos: Double): Boolean =
+    (xPos < this.place.x + side &&
+    xPos > this.place.x - side &&
+    yPos < this.place.y + side &&
+    yPos > this.place.y - side)
+
+  def draw(g: GraphicsContext) =
+    //val oldTransform = g.getTransform
+
+    //g.translate(place.x, place.y)
+    //g.fillRect(side, side, side, side)
+    g.fillRect(place.x-(side/2), place.y-(side/2), side, side)
+
+
+    //g.setTransform(oldTransform)
+
+  end draw
 
 end Goal
