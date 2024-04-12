31636752da8be1d8ced0e86787cc0a82f29bd3d2
Veikko Markkula <veikko.markkula@aalto.fi>
2024-04-12 15:49:13 +0300

    New game mechanics added to the game loop.

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -2,20 +2,39 @@ package scala
 
 import scala.collection.mutable.*
 import scalafx.scene.canvas.GraphicsContext
+import scalafx.scene.paint.Color
 
 object Game:
-  //var enemy = Knight(50,500)
+  val player = Player()
+  // Goals should be placed before enemies, because goals will be used to determine enimies movement.
+  val goals = Buffer[Goal]()
+  goals += Goal(Vector2D(440, 480 ))
+  //goals += Goal(Vector2D(310, 350 ))
+  goals += Goal(Vector2D(480, 220 ))
+  goals += Goal(Vector2D(1040, 220 ))
+  goals += Goal(Vector2D(1040, 1020 ))
 
-  val enemies = Buffer[Knight]()
-  //enemies += Knight(Vector2D(0.5,0.5), Vector2D(100, 100), 0.002)
-  enemies += Knight(Vector2D(0.5,0), Vector2D(50, 480), 0)
-  enemies += Knight(Vector2D(0.5,0), Vector2D(50, 500), 0)
-  enemies += Knight(Vector2D(0.5,0), Vector2D(50, 520), 0)
+  var enemies = Buffer[Knight]()
+  enemies += Knight(Vector2D(0,0), Vector2D(100, 100), 0, goals)
+  enemies += Knight(Vector2D(0.5,0), Vector2D(50, 480), 0, goals)
+  enemies += Knight(Vector2D(0.5,0), Vector2D(50, 500), 0, goals)
+  enemies += Knight(Vector2D(0.5,0), Vector2D(50, 520), 0, goals)
+  enemies += Knight(Vector2D(0.5,0), Vector2D(500, 520), 0, goals)
 
-  //def passTime() =
-  //  enemy.move()
   def step() =
     enemies.foreach(_.move())
+    enemies.foreach(_.updateGoals())
+    player.hp = player.hp - enemies.count(_.enemyGoalReached)
+    enemies = enemies.filterNot(_.enemyGoalReached)
 
-  def draw(g: GraphicsContext) = enemies foreach (_.draw(g))
+  //Game losing conditions
+  def gameLost(): Boolean =
+    player.hp <= 0
+
+  def draw(g: GraphicsContext) =
+    g.fill = Color.LightGreen
+    goals foreach (_.draw(g))
+    g.fill = Color.Red
+    enemies foreach (_.draw(g))
+    //if gameLost then DRAW LOSINGS
 end Game
